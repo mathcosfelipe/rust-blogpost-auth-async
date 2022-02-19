fedf2fc8801f9b0a130cc48bc4e4f8bd88d50384
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2022-02-19 17:14:31 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 use actix_web::{web, App, HttpServer};
 
+#[macro_use]
+extern crate diesel;
+
+use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
 mod handlers;
+mod errors;
+mod models;
+mod schema;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool: Pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
     HttpServer::new(|| {
         App::new()
         .route("/users", web::get().to(handlers::get_users))
